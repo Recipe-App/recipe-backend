31007cd4b9169f02024086fe637c6fc2c6986bbf
learn <learn@learn14.local>
2018-08-02 14:31:06 -0700

    added account_info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user, only: :show
 
   def show
-    user = User.find params[:id]
-    render json: user
+    @user = User.find params[:id]
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pantry_items
   post 'user_token' => 'user_token#create'
-  resources :users
+  resources :users,  defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
