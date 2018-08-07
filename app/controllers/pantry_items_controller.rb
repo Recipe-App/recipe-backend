6b2040ddb797d5629fb617c9315e5a35c46bfe30
learn <learn@learn12.local>
2018-08-07 09:38:32 -0700

    Updated pantry items controller

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,2 +1,19 @@
 class PantryItemsController < ApplicationController
+
+# def show
+#   pantry_item = PantryItem.find params[:id]
+#   render json: pantry_item
+# end
+#
+def create
+  pantry_items = PantryItem.new(pantry_items_params)
+  pantry_items.save
+  render json: pantry_items, status: 201
+
+end
+
+private
+ def pantry_items_params
+   params.require(:pantry_item).permit(:proteins, :veggies, :grains, :seasonings, :other)
+ end
 end
