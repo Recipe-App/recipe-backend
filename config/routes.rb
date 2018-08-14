269af01f897bf8df3c86183c74cb013130457dda
learn <learn@learn14.local>
2018-08-14 11:30:30 -0700

    Got the texting feature to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :saved_recipes,  defaults: {format: :json}
-  resources :pantry_items,  defaults: {format: :json}
-  post 'user_token' => 'user_token#create'
   resources :users,  defaults: {format: :json}
+  post 'user_token' => 'user_token#create'
+
+  resources :pantry_items,  defaults: {format: :json}
+
+  resources :saved_recipes,  defaults: {format: :json}
+  post 'grocery_list' => 'grocery_list#text'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
