33350a635bf281c93124fcaef6132905b1382e33
Carlos Lazo <clazo@berkeley.edu>
2018-11-07 22:06:57 -0800

    made changes

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-    config.account_sid = ENV["twilio_account_sid"]
-    config.auth_token = ENV["twilio_auth_token"]
+    config.account_sid = Rails.application.secrets.twilio_account_sid
+    config.auth_token = Rails.application.secrets.twilio_auth_token
 end
